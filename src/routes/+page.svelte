f5798fb9517d856dbfa043b062e830654ae35e31
Yaoyu Chen <43839115+yych42@users.noreply.github.com>
2024-12-03 13:49:02 +0100

    adjust footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -209,9 +209,19 @@
 			class="container mx-auto flex flex-col space-y-4 lg:flex-row lg:items-center lg:justify-between"
 		>
 			<p class="text-xs text-gray-400 lg:max-w-xl">
-				© 2024 Pedal for Medal Campaign. All rights reserved. Helldivers™ and Helldivers™ 2 are
-				trademarks of Arrowhead Game Studios AB. This campaign is not affiliated with Arrowhead Game
-				Studios.
+				<span class="gap-1 text-yellow-500">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="inline-block h-3 w-3 align-[-2px] text-yellow-500"
+						><path
+							d="M12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22ZM12 20C16.42 20 20 16.42 20 12C20 7.58 16.42 4 12 4C7.58 4 4 7.58 4 12C4 16.42 7.58 20 12 20ZM12 17C10.1805 17 8.58698 16.0265 7.71225 14.5723L9.42649 13.5432C9.95089 14.4162 10.9069 15 12 15C13.6575 15 15 13.6575 15 12C15 10.3425 13.6575 9 12 9C10.9074 9 9.95162 9.58339 9.42709 10.4558L7.71225 9.42771C8.58698 7.9735 10.1805 7 12 7C14.76 7 17 9.24 17 12C17 14.76 14.76 17 12 17Z"
+						></path></svg
+					>
+					Pedal for Medal Campaign. No rights reserved. Yup. This is a fun project.</span
+				> Helldivers™ and Helldivers™ 2 are trademarks of Arrowhead Game Studios AB. This campaign
+				is not affiliated with Arrowhead Game Studios.
 			</p>
 			<nav class="flex space-x-4">
 				<a
