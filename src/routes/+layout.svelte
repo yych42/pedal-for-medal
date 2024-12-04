0bd446aacdcfb075e67e57fcb8ed07cb61209980
Yaoyu Chen <43839115+yych42@users.noreply.github.com>
2024-12-04 13:49:02 +0100

    add meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,12 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Pedal for Medal</title>
+	<meta
+		name="description"
+		content="Join the petition campaign to bring bicycles to Helldivers 2. Because nothing says 'Super Earth' like a bike bell in the face of alien tyranny!"
+	/>
+	<meta property="og:image" content="/bike-hero.jpeg" />
+</svelte:head>
 {@render children()}
